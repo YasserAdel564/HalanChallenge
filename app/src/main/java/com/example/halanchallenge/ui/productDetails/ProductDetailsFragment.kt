c8a6923652a338ebd18ccc88783743852f5632fd
Yasser Adel <yasser.adel@hmaserv.com>
2022-03-28 15:59:41 +0200

    Add Product's Price in Details

--- app/src/main/java/com/example/halanchallenge/ui/productDetails/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/halanchallenge/ui/productDetails/ProductDetailsFragment.kt
@@ -8,18 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.halanchallenge.R
 import com.example.halanchallenge.data.model.product.ProductInfo
 import com.example.halanchallenge.databinding.ActivityProductDetailsBinding
 import com.example.halanchallenge.ui.SharedVM
-import java.util.*
-import kotlin.concurrent.timerTask
 
 class ProductDetailsFragment : Fragment() {
 
     private lateinit var binding: ActivityProductDetailsBinding
     private val sharedVM: SharedVM by activityViewModels()
     private val imagesAdapter = ProductImagesAdapter()
-    private var timer: Timer = Timer()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +37,7 @@ class ProductDetailsFragment : Fragment() {
         binding.productDescriptionTv.text = productInfo.deal_description
         binding.productDescriptionTv.movementMethod = ScrollingMovementMethod();
         binding.productTitleTv.text = productInfo.name_ar
-        binding.productPriceTv.text = productInfo.price.toString()
+        binding.productPriceTv.text = productInfo.price.toString() +requireActivity().resources.getString(R.string.epg)
         productInfo.images?.let { imagesAdapter.replaceData(it) }
         binding.productImagesBanner.adapter = imagesAdapter;
         binding.arIndicator.attachTo(binding.productImagesBanner, true);
--- app/src/main/java/com/example/halanchallenge/ui/productDetails/ProductImagesAdapter.kt
+++ app/src/main/java/com/example/halanchallenge/ui/productDetails/ProductImagesAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.halanchallenge.ui.productDetails
 
-import android.view.View
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
